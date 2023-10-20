5a50302c4acdd3c80d928c156c6878b16e22bdb2
ClaraR93 <>
2023-10-20 15:57:47 +0000

    Final commit of the day

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,6 +12,7 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
+    print(uri)
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
